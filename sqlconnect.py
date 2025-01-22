27e496fb67227ad362bfaf3c69c1ae77cd32159d
KihoroWangui <94446783+KihoroWangui@users.noreply.github.com>
2025-01-22 17:05:55 +0300

    sqlconnect
    connecting to sql db via python

---
+++ sqlconnect.py
@@ -0,0 +1,16 @@
+import oracledb
+
+# Connect to Oracle
+connection = oracledb.connect(
+    user="Hannah",
+    password="tuk123",
+    dsn=""
+)
+
+# Query the database
+cursor = connection.cursor()
+cursor.execute("SELECT * Hannah")
+for row in cursor:
+    print(row)
+
+connection.close()
